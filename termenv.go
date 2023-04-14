X-oss-byte/termenv

commit 1111971edffa5f9ed2a38bd57f7e434b424f7b29

Don't run OSC queries on CI

termenv.go
@@ -22,10 +22,18 @@ const (
 	TrueColor
 )
 
+func isTTY(fd uintptr) bool {
+	if len(os.Getenv("CI")) > 0 {
+		return false
+	}
+
+	return isatty.IsTerminal(fd)
+}
+
 // ColorProfile returns the supported color profile:
 // Ascii, ANSI, ANSI256, or TrueColor.
 func ColorProfile() Profile {
-	if !isatty.IsTerminal(os.Stdout.Fd()) {
+	if !isTTY(os.Stdout.Fd()) {
 		return Ascii
 	}
 
@@ -34,7 +42,7 @@ func ColorProfile() Profile {
 
 // ForegroundColor returns the terminal's default foreground color.
 func ForegroundColor() Color {
-	if !isatty.IsTerminal(os.Stdout.Fd()) {
+	if !isTTY(os.Stdout.Fd()) {
 		return NoColor{}
 	}
 
@@ -43,7 +51,7 @@ func ForegroundColor() Color {
 
 // BackgroundColor returns the terminal's default background color.
 func BackgroundColor() Color {
-	if !isatty.IsTerminal(os.Stdout.Fd()) {
+	if !isTTY(os.Stdout.Fd()) {
 		return NoColor{}
 	}
 

commit cb48c33ded1aeeaad4695aeb55749f4e479dc871

fix: remove CI exception
Instead of making an exception to the CI environment variable, make
Termenv aware of TERMENV_TTY and assume tty using environment variables.

Users who use the CI environment variable should export TERMENV_TTY
instead using something like `export TERMENV_TTY=$CI`

Fixes: 1111971edffa ("Don't run OSC queries on CI")

termenv.go
@@ -3,6 +3,7 @@ package termenv
 import (
 	"errors"
 	"os"
+	"strings"
 
 	"github.com/mattn/go-isatty"
 )
@@ -29,9 +30,14 @@ func (o *Output) isTTY() bool {
 	if o.assumeTTY || o.unsafe {
 		return true
 	}
-	if len(o.environ.Getenv("CI")) > 0 {
+	env := o.environ.Getenv("TERMENV_TTY")
+	switch strings.ToLower(env) {
+	case "1", "true", "yes", "on":
+		return true
+	case "0", "false", "no", "off":
 		return false
 	}
+
 	if f, ok := o.Writer().(*os.File); ok {
 		return isatty.IsTerminal(f.Fd())
 	}